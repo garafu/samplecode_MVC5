36c6b27937989c7ed12b2c3815e46682c6cda042
garafu <garafo@gmail.com>
2015-05-16 15:47:28 +0900

    first commit

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication.Controllers
+{
+    using System.Web.Mvc;
+    using WebApplication.Models;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var model = new Greeting();
+            
+            model.Message = "Hello World !";
+
+            return this.View(model);
+        }
+    }
+}
---
+++ WebApplication/Models/Greeting.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication.Models
+{
+    /// <summary>
+    /// 挨拶を表現するモデル
+    /// </summary>
+    public class Greeting
+    {
+        /// <summary>
+        /// メッセージを取得または設定します。
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
